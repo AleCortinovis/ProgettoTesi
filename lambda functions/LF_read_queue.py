c75d75ec0b44d0ea0c20dd1a5b4d1e53f5f0b185
AleCortinovis <114218535+AleCortinovis@users.noreply.github.com>
2024-06-15 21:02:01 +0200

    Update LF_read_queue.py

--- lambda functions/LF_read_queue.py
+++ lambda functions/LF_read_queue.py
@@ -1,7 +1,8 @@
 import boto3
 
+client = boto3.client("sqs")
+
 def my_handler(event, context):
-    #print(event)
     
     #riceve messaggi da coda test
     records = event['Records']
@@ -11,7 +12,6 @@ def my_handler(event, context):
         body = record['body']
         print(body)
         
-        client = boto3.client("sqs")
         response = client.send_message(
             QueueUrl="https://sqs.us-east-1.amazonaws.com/760507411025/coda-link",
             MessageBody=body)
